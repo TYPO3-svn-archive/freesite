3975054bf1d1a1a2437312badccef3f198015f5c
inshadow <inshadow@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-04 09:48:13 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/freesite/trunk@30748 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "freesite".
+#
+# Auto generated 02-03-2010 15:14
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Freesite',
+	'description' => 'A module which automates the creation of a new standard template websites including a backend user, filemounts and dummy page set.',
+	'category' => 'module',
+	'shy' => 0,
+	'version' => '0.2.3',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'mod',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Stig NÃ¸rgaard FÃ¦rch',
+	'author_email' => 'stig@8620.dk',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+			'cms' => '',
+			'tstemplate_ceditor' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:56:{s:9:"_todo.txt";s:4:"731e";s:21:"ext_conf_template.txt";s:4:"d490";s:12:"ext_icon.gif";s:4:"465b";s:14:"ext_tables.php";s:4:"5926";s:14:"doc/manual.sxw";s:4:"9446";s:22:"mod/class.freesite.php";s:4:"f2bc";s:13:"mod/clear.gif";s:4:"cc11";s:12:"mod/conf.php";s:4:"8141";s:16:"mod/freesite.gif";s:4:"465b";s:13:"mod/index.php";s:4:"6eec";s:21:"mod/locallang_mod.php";s:4:"ed46";s:17:"mod/template.html";s:4:"7d79";s:25:"mod/template_created.html";s:4:"8662";s:29:"mod/template_selectPages.html";s:4:"b906";s:32:"mod/template_selectTemplate.html";s:4:"fb61";s:17:"mod/titleshow.gif";s:4:"1242";s:17:"mod/typo3logo.gif";s:4:"05bc";s:21:"mod/tmplimages/10.gif";s:4:"426b";s:21:"mod/tmplimages/11.gif";s:4:"6df9";s:21:"mod/tmplimages/12.gif";s:4:"9403";s:21:"mod/tmplimages/13.gif";s:4:"4a6a";s:21:"mod/tmplimages/14.gif";s:4:"e15d";s:21:"mod/tmplimages/15.gif";s:4:"b8fd";s:21:"mod/tmplimages/16.gif";s:4:"e292";s:21:"mod/tmplimages/17.gif";s:4:"5deb";s:21:"mod/tmplimages/18.gif";s:4:"e662";s:21:"mod/tmplimages/19.gif";s:4:"929a";s:20:"mod/tmplimages/2.gif";s:4:"aa41";s:21:"mod/tmplimages/20.gif";s:4:"4997";s:21:"mod/tmplimages/21.gif";s:4:"6b67";s:21:"mod/tmplimages/22.gif";s:4:"9e05";s:21:"mod/tmplimages/23.gif";s:4:"5d54";s:21:"mod/tmplimages/24.gif";s:4:"dd6b";s:21:"mod/tmplimages/25.gif";s:4:"d2e9";s:20:"mod/tmplimages/3.gif";s:4:"8a1c";s:21:"mod/tmplimages/30.gif";s:4:"b129";s:21:"mod/tmplimages/31.gif";s:4:"604f";s:21:"mod/tmplimages/32.gif";s:4:"c8a1";s:21:"mod/tmplimages/33.gif";s:4:"ad9e";s:21:"mod/tmplimages/34.gif";s:4:"8489";s:21:"mod/tmplimages/35.gif";s:4:"438b";s:21:"mod/tmplimages/36.gif";s:4:"4d96";s:21:"mod/tmplimages/37.gif";s:4:"ea5f";s:21:"mod/tmplimages/38.gif";s:4:"8385";s:21:"mod/tmplimages/39.gif";s:4:"6f96";s:20:"mod/tmplimages/4.gif";s:4:"32d3";s:21:"mod/tmplimages/40.gif";s:4:"4b0d";s:21:"mod/tmplimages/41.gif";s:4:"63f6";s:21:"mod/tmplimages/42.gif";s:4:"5ce4";s:21:"mod/tmplimages/43.gif";s:4:"b031";s:21:"mod/tmplimages/44.gif";s:4:"c014";s:20:"mod/tmplimages/5.gif";s:4:"7389";s:20:"mod/tmplimages/6.gif";s:4:"9559";s:20:"mod/tmplimages/7.gif";s:4:"a261";s:20:"mod/tmplimages/8.gif";s:4:"d9c1";s:20:"mod/tmplimages/9.gif";s:4:"f1aa";}',
+);
+
+?>
